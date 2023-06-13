400e1251602dc7b27cb312dd49079fab7627f09d
Sanjay Tiwari <sanjutiwari9559@gmail.com>
2023-06-13 17:36:34 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
     return (
-        <><h1>hlo world</h1>
+        <><h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta blanditiis odio facere a obcaecati aperiam officia ea, unde ipsam, aliquam voluptate quibusdam cum totam voluptates earum perferendis quisquam quasi laborum. </h1>
         </>
     );
 }
